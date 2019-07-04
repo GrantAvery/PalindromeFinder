835de1c39507c15ed8b600c5633067be1afbe535
Grant Avery <grant.avery@spectrumhealth.org>
2019-07-04 14:56:24 -0400

    Initial commit

---
+++ PalindromeFinder/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PalindromeFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            // Uses args if they exist, but if not it has a more verbose option to ask for input
+            if (args != null && args.Length > 0 && args[0] != null)
+            {
+                Console.WriteLine(isPalindrome(args[0]));
+            }
+            else
+            {
+                Console.WriteLine("Welcome! Please enter text and then hit Enter. " +
+                    "I will tell you whether the text is a palindrome.");
+                input = Console.ReadLine();
+
+                bool response = isPalindrome(input);
+
+                if (response)
+                {
+                    Console.WriteLine($"\"{input}\" is a palindrome!");
+
+                }
+                else
+                {
+                    Console.WriteLine($"\"{input}\" is not a palindrome!");
+                }
+            }
+        }
+
+        public static bool isPalindrome(string input)
+        {
+            // Regex explanation:
+            // ^ is equivalent to "not" when inside the []
+            // \p{L} finds all unicode letters according to Regex, but nothing else
+            // + checks all characters in the string, not just the first
+            // In all, it finds all non-letters and removes them
+            string validInput = Regex.Replace(input, @"[^\p{L}]+", "").ToLower();
+
+            for (int j = 0; j < (validInput.Length / 2); j++)
+            {
+                if (validInput[j] == validInput[validInput.Length - j - 1])
+                {
+                    // The letters on each side match, so far so good
+                }
+                else
+                {
+                    // Not a palindrome
+                    return false;
+                }
+            }
+
+            // If we've gotten here, all the opposite letters match, aka it is a palindrome
+            return true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+<h1 align="center">
+  PalindromeFinder
+</h1>
+
+### Simple C# console app to inform the user whether some text input is a palindrome
+
+Another in a series of simple programs to keep my coding skills fresh. Built using .NET Core 2.2.
+
+//TODO: make tests
+
+## Build:
+
+Navigate to {baseRepo}/PalindromeFinder in the console app of your choice
+
+```
+$ csc Program.cs
+```
+
+## Run:
+### On PC
+
+```
+$ Program
+```
+
+### On Mac
+
+You'll need [mono](https://www.mono-project.com/) installed but otherwise it isn't much more complicated.
+
+```
+$ mono Program.exe
+```
